7e99a359f9f2088b1f8c5f011b25b2f2066904aa
Max <maxime.delannoy@outlook.com>
2020-10-28 16:07:43 +0100

    search bar ok

--- src/component/Filmchoice.jsx
+++ src/component/Filmchoice.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import './Filmchoice.css';
 
-function Filmchoice() {
-  return (
-    <form className="searchbar">
-      <input
-        className="bar"
-        type="text"
-        placeholder="Je sais ce que je veux !"
-      />
-    </form>
-  );
-}
+class Filmchoice extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+    this.movieSearch = this.movieSearch.bind(this);
+  }
 
+  movieSearch(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  render() {
+    const { value } = this.state;
+    return (
+      <form className="searchbar">
+        <input
+          className="bar"
+          type="text"
+          placeholder="Je sais ce que je veux !"
+          value={value}
+          onChange={this.movieSearch}
+        />
+      </form>
+    );
+  }
+}
 export default Filmchoice;
