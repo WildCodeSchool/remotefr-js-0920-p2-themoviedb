886f7c199bab8290ac4364f451241b4062da7d46
Max <maxime.delannoy@outlook.com>
2020-10-28 16:43:37 +0100

    gitignore modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import Filmchoice from './component/Filmchoice';
 import Selection from './component/Selection';
+import Selected from './component/Selected';
 
 function App() {
   return (
     <div className="research">
       <Filmchoice />
+      <Selected />
       <article>
         <Selection />
         <Selection />
