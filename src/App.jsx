d22e357e07b918f50a84708288e08479d0012093
Max <maxime.delannoy@outlook.com>
2020-10-29 15:14:41 +0100

    searchbar working

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import './App.css';
 import Filmchoice from './component/Filmchoice';
 import Selection from './component/Selection';
-import Selected from './component/Selected';
 
 function App() {
   return (
     <div className="research">
       <Filmchoice />
-      <Selected />
+
       <article>
         <Selection />
         <Selection />
--- src/component/Filmchoice.jsx
+++ src/component/Filmchoice.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Filmchoice.css';
+import axios from 'axios';
+import Selected from './Selected';
 
 class Filmchoice extends React.Component {
   constructor(props) {
@@ -7,6 +9,19 @@ class Filmchoice extends React.Component {
     this.state = { value: '' };
     this.movieSearch = this.movieSearch.bind(this);
   }
+  /** deuxième clès resolve ou movie
+   * intialiser avec un tableau vide
+   * enturer formulaire ave div  et map sur le tableau dans le state
+   */
+
+  fetchMovie = (event) => {
+    event.preventDefault();
+    const { value } = this.state;
+    const url = `https://api.themoviedb.org/3/search/movie?query=${value}&api_key=74d5a2d9e7e8b509e4235d8ff4524d48`;
+    axios.get(url).then((response) => {
+      return response.data;
+    });
+  };
 
   movieSearch(event) {
     this.setState({ value: event.target.value });
@@ -15,15 +30,20 @@ class Filmchoice extends React.Component {
   render() {
     const { value } = this.state;
     return (
-      <form className="searchbar">
-        <input
-          className="bar"
-          type="text"
-          placeholder="Je sais ce que je veux !"
-          value={value}
-          onChange={this.movieSearch}
-        />
-      </form>
+      <div className="selection">
+        <form className="searchbar" onSubmit={this.fetchMovie}>
+          <input
+            className="bar"
+            type="text"
+            placeholder="Je sais ce que je veux !"
+            value={value}
+            onChange={this.movieSearch}
+          />
+        </form>
+        <cover className="view">
+          <Selected />
+        </cover>
+      </div>
     );
   }
 }
