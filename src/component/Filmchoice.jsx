c2908187edfa9d972f93ee209fb5da4a5fadae12
Max <maxime.delannoy@outlook.com>
2020-10-30 11:41:32 +0100

    searchBar working

--- src/component/Filmchoice.jsx
+++ src/component/Filmchoice.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Filmchoice.css';
 import axios from 'axios';
-import Selected from './Selected';
+import apiKey from './apiKey';
 
 class Filmchoice extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { value: '' };
+    this.state = { value: '', results: [] };
     this.movieSearch = this.movieSearch.bind(this);
   }
   /** deuxième clès resolve ou movie
@@ -17,9 +17,9 @@ class Filmchoice extends React.Component {
   fetchMovie = (event) => {
     event.preventDefault();
     const { value } = this.state;
-    const url = `https://api.themoviedb.org/3/search/movie?query=${value}&api_key=74d5a2d9e7e8b509e4235d8ff4524d48`;
+    const url = `https://api.themoviedb.org/3/search/movie?query=${value}&api_key=${apiKey}`;
     axios.get(url).then((response) => {
-      return response.data;
+      this.setState({ results: response.data.results });
     });
   };
 
@@ -28,7 +28,8 @@ class Filmchoice extends React.Component {
   }
 
   render() {
-    const { value } = this.state;
+    const { value, results } = this.state;
+
     return (
       <div className="selection">
         <form className="searchbar" onSubmit={this.fetchMovie}>
@@ -41,7 +42,16 @@ class Filmchoice extends React.Component {
           />
         </form>
         <cover className="view">
-          <Selected />
+          {results.map((resultat) => (
+            <result className="resultat">
+              <img
+                alt="filmcover"
+                className="mini"
+                src={`https://image.tmdb.org/t/p/w440_and_h660_face${resultat.poster_path}`}
+              />
+              <h3 className="titre">{resultat.original_title}</h3>
+            </result>
+          ))}
         </cover>
       </div>
     );
