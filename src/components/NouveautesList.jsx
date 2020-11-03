df7dbf1589b4daadd07ea3ac8f9484bdd84a7a04
Valerie TV <vtylskivincent@gmail.com>
2020-11-03 18:26:14 +0100

    update the liste of the films, thanks to the category buttons

--- src/components/NouveautesList.jsx
+++ src/components/NouveautesList.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import axios from 'axios';
-import PropTypes from 'prop-types';
 import Nouveautes from './Nouveautes';
 import styles from './NouveautesList.module.css';
 import apiKey from './apiKey';
@@ -10,7 +9,9 @@ class NouveautesList extends React.Component {
     super(props);
     this.state = {
       listNewMovies: [],
+      chosenValue: '',
     };
+    this.chosenGenre = this.chosenGenre.bind(this);
   }
 
   componentDidMount() {
@@ -18,8 +19,8 @@ class NouveautesList extends React.Component {
   }
 
   fetchNouveautes = () => {
-    const currentYear = new Date().getFullYear();
-    const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=fr-FR&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&year=${currentYear}&with_genres=9648`;
+    const { currentYear } = new Date().getFullYear();
+    const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=fr-FR&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&year=${currentYear}`;
     axios
       .get(url)
       .then((response) => response.data.results)
@@ -29,6 +30,22 @@ class NouveautesList extends React.Component {
         });
       });
   };
+
+  chosenGenre(click) {
+    const { currentYear } = new Date().getFullYear();
+    const { chosenValue } = this.state;
+    const genreMovies = `&with_genres=${chosenValue}`;
+    this.setState({ chosenValue: click.target.value });
+    const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=fr-FR&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&year=${currentYear}${genreMovies}`;
+    axios
+      .get(url)
+      .then((response) => response.data.results)
+      .then((nouveautesArray) => {
+        this.setState({
+          listNewMovies: nouveautesArray,
+        });
+      });
+  }
 
   render() {
     const { listNewMovies } = this.state;
@@ -45,61 +62,61 @@ class NouveautesList extends React.Component {
           ))}
           <div className={styles.divButtons}>
             <p>Je veux voir les nouveautés en&nbsp;:</p>
-            <button type="button" value="28">
+            <button type="button" value="28" onClick={this.chosenGenre}>
               Action
             </button>
-            <button type="button" value="12">
+            <button type="button" value="12" onClick={this.chosenGenre}>
               Aventure
             </button>
-            <button type="button" value="16">
+            <button type="button" value="16" onClick={this.chosenGenre}>
               Animation
             </button>
-            <button type="button" value="35">
+            <button type="button" value="35" onClick={this.chosenGenre}>
               Comédie
             </button>
-            <button type="button" value="80">
+            <button type="button" value="80" onClick={this.chosenGenre}>
               Crime
             </button>
-            <button type="button" value="99">
+            <button type="button" value="99" onClick={this.chosenGenre}>
               Documentaire
             </button>
-            <button type="button" value="18">
+            <button type="button" value="18" onClick={this.chosenGenre}>
               Drame
             </button>
-            <button type="button" value="10751">
+            <button type="button" value="10751" onClick={this.chosenGenre}>
               Familial
             </button>
-            <button type="button" value="14">
+            <button type="button" value="14" onClick={this.chosenGenre}>
               Fantastique
             </button>
-            <button type="button" value="36">
+            <button type="button" value="36" onClick={this.chosenGenre}>
               Histoire
             </button>
-            <button type="button" value="27">
+            <button type="button" value="27" onClick={this.chosenGenre}>
               Horreur
             </button>
-            <button type="button" value="10402">
+            <button type="button" value="10402" onClick={this.chosenGenre}>
               Musique
             </button>
-            <button type="button" value="9648">
+            <button type="button" value="9648" onClick={this.chosenGenre}>
               Mystère
             </button>
-            <button type="button" value="10749">
+            <button type="button" value="10749" onClick={this.chosenGenre}>
               Romance
             </button>
-            <button type="button" value="878">
+            <button type="button" value="878" onClick={this.chosenGenre}>
               S.F.
             </button>
-            <button type="button" value="10770">
+            <button type="button" value="10770" onClick={this.chosenGenre}>
               Téléfilm
             </button>
-            <button type="button" value="53">
+            <button type="button" value="53" onClick={this.chosenGenre}>
               Thriller
             </button>
-            <button type="button" value="10752">
+            <button type="button" value="10752" onClick={this.chosenGenre}>
               Guerre
             </button>
-            <button type="button" value="37">
+            <button type="button" value="37" onClick={this.chosenGenre}>
               Western
             </button>
           </div>
@@ -109,16 +126,16 @@ class NouveautesList extends React.Component {
   }
 }
 
-NouveautesList.propTypes = {
-  listNewMovies: PropTypes.arrayOf(
-    PropTypes.shape({ title: PropTypes.string, posterPath: PropTypes.string })
-      .isRequired,
-  ),
-};
+// NouveautesList.propTypes = {
+//   listNewMovies: PropTypes.arrayOf(
+//     PropTypes.shape({ title: PropTypes.string, posterPath: PropTypes.string })
+//       .isRequired,
+//   ),
+// };
 
-NouveautesList.defaultProps = {
-  listNewMovies: [],
-};
+// NouveautesList.defaultProps = {
+//   listNewMovies: [],
+// };
 //   title: '',
 //   posterPath: 'https://via.placeholder.com/190x285.png',
 // };
