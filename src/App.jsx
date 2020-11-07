8342f165bb4b147c1db11a6ab8c93ff0e165b138
Valerie TV <vtylskivincent@gmail.com>
2020-11-07 20:04:07 +0100

    modify class of global div in App.js

--- src/App.jsx
+++ src/App.jsx
@@ -55,7 +55,7 @@ class App extends React.Component {
     const { startTime, endTime, runtime } = this.state;
     const { listNewMovies } = this.state;
     return (
-      <div className="homepage">
+      <div className={styles.content}>
         <Header />
         <Switch>
           <Route path="/filter-by-duration">
